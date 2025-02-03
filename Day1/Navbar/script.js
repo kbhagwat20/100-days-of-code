46f77225abbe5fbd8a7c0ab6eb4610514cd13d19
Krupali Bhagwat <bhagwatkrupali15@gmail.com>
2025-02-03 18:40:51 +0530

    Day 1: Responsive Navbar

---
+++ Day1/Navbar/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.classList.toggle('active'); // Toggles the "active" class
+});
